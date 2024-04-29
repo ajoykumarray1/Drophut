41d8c40984390fa2affdb5e287bcdf8273345679
Ajoy Kumar Ray <ajoy25695@gmail.com>
2024-04-29 23:12:52 +0600

    blog template

--- droneEcommerce/urls.py
+++ droneEcommerce/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("pages.urls")),
     path('', include("accounts.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    path('', include("blog.urls")),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
